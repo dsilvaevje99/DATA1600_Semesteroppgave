29513bddf108fc8a542a3cf03dc5bccf6b8a00e5
Daniel Silva <daniel@visualyst.co>
2020-04-02 18:40:40 +0200

    Initial pages created.
    Most necessary components added. Design might change slightly.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
+    public void saveUserConfig(ActionEvent actionEvent) {
+    }
+
+    public void keyboardYesClicked(ActionEvent actionEvent) {
+    }
+
+    public void keyboardNoClicked(ActionEvent actionEvent) {
+    }
+
+    public void mouseYesClicked(ActionEvent actionEvent) {
+    }
+
+    public void mouseNoClicked(ActionEvent actionEvent) {
+    }
+
+    public void createNewConfig(ActionEvent actionEvent) {
+    }
+
+    public void login(ActionEvent actionEvent) {
+    }
+
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    public void createNewComponent(ActionEvent actionEvent) {
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("User Page.fxml"));
         primaryStage.setTitle("DATA1600 Semesteroppgave");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
