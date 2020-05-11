1c9786d70e0681f43fb09cd719af33bdf34b33f3
Daniel Silva <daniel@visualyst.co>
2020-05-11 15:07:31 +0200

    Added input validation with error messages when creating new component.

--- src/sample/ComponentController.java
+++ src/sample/ComponentController.java
@@ -15,6 +15,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.regex.Pattern;
 
 public class ComponentController {
     public ChoiceBox<String> compTypeChoiceBox;
@@ -33,16 +34,49 @@ public class ComponentController {
     }
 
     public void createNewComponent(ActionEvent actionEvent) throws IOException {
-        //ERROR HANDLING: validate input values? Display errors in compErrorLabel
-        //create string from input values
-        String newComponentString = System.lineSeparator()+compTypeChoiceBox.getValue()+"|"+compNameField.getText()+"|"+compPriceField.getText();
-        //save to file
-        Writer output;
-        output = new BufferedWriter(new FileWriter("./src/sample/Admin Files/components.txt", true));
-        output.append(newComponentString);
-        output.close();
-        //return to admin page
-        openAdminScene(actionEvent);
+        //Save input values to variables
+        String componentType = compTypeChoiceBox.getValue();
+        String componentName = compNameField.getText();
+        String componentPrice = compPriceField.getText();
+
+        //ERROR HANDLING: Make sure all fields have a value. If they don't have one, set the text of the error label and stop executing method.
+        if(componentType.equals("") || componentName.equals("") || componentPrice.equals("")) {
+            compErrorLabel.setText("All fields must be filled out!");
+            return;
+        }
+
+        //ERROR HANDLING: make sure price string is a valid double
+        boolean priceCheck;
+        try {
+            Double.parseDouble(componentPrice);
+            priceCheck = true;
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+            priceCheck = false;
+        }
+        if(priceCheck) {
+            //ERROR HANDLING: make sure all strings match the regex
+            String componentTypeRegex = "[a-zA-Z '\\-]{3,20}";
+            String componentNameRegex = "[\"a-zA-Z 0-9()'\\-]{3,40}";
+            String componentPriceRegex = "[0-9.]{1,6}";
+            if(Pattern.matches(componentTypeRegex, componentType) && Pattern.matches(componentNameRegex, componentName) && Pattern.matches(componentPriceRegex, componentPrice)) {
+
+                //If everything is OK, create string from input values
+                String newComponentString = System.lineSeparator()+componentType+"|"+componentName+"|"+componentPrice;
+                //save to file
+                Writer output;
+                output = new BufferedWriter(new FileWriter("./src/sample/Admin Files/components.txt", true));
+                output.append(newComponentString);
+                output.close();
+                //return to admin page
+                openAdminScene(actionEvent);
+
+            } else {
+                compErrorLabel.setText("Invalid input value(s)!");
+            }
+        } else {
+            compErrorLabel.setText("Price must be a valid number!");
+        }
     }
 
 }
