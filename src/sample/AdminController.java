d0c9fd8d781e0918476399f7c7d15df92aacccff
Daniel Silva <daniel@visualyst.co>
2020-05-14 16:29:40 +0200

    Added components on admin page loading in thread with delay.

--- src/sample/AdminController.java
+++ src/sample/AdminController.java
@@ -1,14 +1,12 @@
 package sample;
 
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +19,11 @@ public class AdminController {
     public Button logOutButton;
     public ChoiceBox compChoiceBox;
     public TextField deleteSearchField;
+    public Button viewCompButton;
+    public Button deleteCompButton;
+    public Label loadErrorLabel;
+
+    private ComponentThread task;
 
     public void logOut(ActionEvent actionEvent) throws IOException {
         //Log out code
@@ -42,11 +45,49 @@ public class AdminController {
     }
 
     public void viewComponents(ActionEvent actionEvent) {
-        ComponentManager componentManager = new ComponentManager();
-        List<Component> sortedList = componentManager.refineList("All");
-        //populate list with results here
+        task = new ComponentThread();
+        task.setOnSucceeded(this::threadDone);
+        task.setOnFailed(this::threadFailed);
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        newComponentButton.setDisable(true);
+        adminTableView.setDisable(true);
+        logOutButton.setDisable(true);
+        compChoiceBox.setDisable(true);
+        deleteSearchField.setDisable(true);
+        viewCompButton.setDisable(true);
+        deleteCompButton.setDisable(true);
+        th.start();
+    }
+
+    private void threadDone(WorkerStateEvent e) {
+        List<Component> sortedList = task.getValue();
+        //populate list with results from thread
         adminTableView.getItems().clear();
         adminTableView.getItems().addAll(sortedList);
+
+        newComponentButton.setDisable(false);
+        adminTableView.setDisable(false);
+        logOutButton.setDisable(false);
+        compChoiceBox.setDisable(false);
+        deleteSearchField.setDisable(false);
+        viewCompButton.setDisable(false);
+        deleteCompButton.setDisable(false);
+    }
+
+    private void threadFailed(WorkerStateEvent event) {
+        var e = event.getSource().getException();
+        // Print error and show error message to user
+        System.out.println("ERROR: Couldnt load components into admin page: "+e.getMessage());
+        loadErrorLabel.setText("An error occured when loading components!");
+
+        newComponentButton.setDisable(false);
+        adminTableView.setDisable(false);
+        logOutButton.setDisable(false);
+        compChoiceBox.setDisable(false);
+        deleteSearchField.setDisable(false);
+        viewCompButton.setDisable(false);
+        deleteCompButton.setDisable(false);
     }
 
     public void deleteComponent(ActionEvent actionEvent) throws IOException, ClassNotFoundException {
